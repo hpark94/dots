73ab551426c407ad637946fa452fdab2b64510ec
Hyeong-Gyu Park <hyeonggyu94@googlemail.com>
2026-05-08 20:09:57 +0200

    Feat: etree now ignores some directories

--- .aliases.sh
+++ .aliases.sh
@@ -1,5 +1,5 @@
 _has() { command -v "$1" >/dev/null 2>&1; }
 _has eza && alias ll="eza --color=always --icons=always --sort=type -alF"
-_has eza && alias etree="eza --color=always --icons=always --tree -a"
+_has eza && alias etree="eza --color=always --icons=always --tree -a -I '.git|node_modules|.venv|__pycache__|_minted*'"
 _has lazygit && alias lg="lazygit"
 alias standby="systemctl suspend"
