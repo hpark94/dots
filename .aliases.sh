08bf05703973d07a0dc0b558a3b5f3250b63737c
Hyeong-Gyu Park <hyeonggyu94@googlemail.com>
2026-05-11 00:49:51 +0200

    Feat: ll now also works without eza

--- .aliases.sh
+++ .aliases.sh
@@ -1,5 +1,9 @@
 _has() { command -v "$1" >/dev/null 2>&1; }
-_has eza && alias ll="eza --color=always --icons=always --sort=type -alF"
+if _has eza; then
+    alias ll="eza --color=always --icons=always --sort=type -alF"
+else
+    alias ll="ls -laF"
+fi
 _has eza && alias etree="eza --color=always --icons=always --tree -a -I '.git|node_modules|.venv|__pycache__|_minted*'"
 _has lazygit && alias lg="lazygit"
 alias standby="systemctl suspend"
