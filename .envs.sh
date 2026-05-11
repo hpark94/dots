2a4b2025f36b361d7e37375c3a0f8c485c762c07
Hyeong-Gyu Park <hyeonggyu94@googlemail.com>
2026-05-11 22:29:08 +0200

    Fix: not loading envs in ssh sessions

--- .envs.sh
+++ .envs.sh
@@ -1,9 +1,12 @@
 export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:$HOME/.local/scripts:$PATH
 export PATH="$HOME/go/bin:$PATH"
-export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
-export OMPI_CXX=g++
-export QT_QPA_PLATFORM=wayland
-export LIBVIRT_DEFAULT_URI="qemu:///system"
+
+if [[ -z "$SSH_CONNECTION" && -z "$SSH_TTY" ]]; then
+    export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+    export OMPI_CXX=g++
+    export QT_QPA_PLATFORM=wayland
+    export LIBVIRT_DEFAULT_URI="qemu:///system"
+fi
 
 if [[ -f "${HOME}/.env" ]]; then
     source "${HOME}/.env"
