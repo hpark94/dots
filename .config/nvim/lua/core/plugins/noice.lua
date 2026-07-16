f1dbd32f9002c08f081d996a877f114e363808c8
Hyeong-Gyu Park <hyeonggyu94@googlemail.com>
2026-07-16 12:45:03 +0200

    chore: add noice to nvim again

---
+++ .config/nvim/lua/core/plugins/noice.lua
@@ -0,0 +1,28 @@
+return {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+            },
+        },
+        presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = true,
+        },
+        routes = {
+            {
+                view = "notify",
+                filter = { event = "msg_showmode" },
+            },
+        },
+    },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+}
